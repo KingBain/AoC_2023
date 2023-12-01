7ce00d7706c747757a4154c34410ab672ac5ea52
John Bain <john.bain@gmail.com>
2023-12-01 13:47:43 +0000

    stage 1, easy

--- Day1/stage1.py
+++ Day1/stage1.py
@@ -1,1 +1,22 @@
-test
+import re
+#read file into for loop 
+
+total = 0
+
+with open("data.txt") as file:
+  for item in file:
+    #search/filter for numerals, probably a regex,
+    #create new array 
+    numbers = re.findall(r'\d', item)
+    
+    #find first and last digit, combine them into large int
+    #print(int(numbers[0]+numbers[-1]))
+    
+    #sum all lines together
+    total += int(numbers[0]+numbers[-1])
+
+print (total)
+
+
+
+#output sum 
