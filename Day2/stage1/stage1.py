9a16e7b3b97b690cb5e71035bea6e5ffbf87f5a3
John Bain <john.bain@gmail.com>
2023-12-02 21:24:39 +0000

    restores stage1 to broken file

--- Day2/stage1/stage1.py
+++ Day2/stage1/stage1.py
@@ -1,9 +1,15 @@
 import re
 
-total = 0
+limit = {
+    "red" : 12,
+    "green" : 13,
+    "blue" : 14 
+}
+
 gameRoster = {}
 gameIDPattern = r'^Game (\d+):'
 colourAndNumberPattern = r'(?P<number>\d+) (?P<color>\w+)'
+
 
 with open("data.txt") as file:
   for item in file:
@@ -11,19 +17,35 @@ with open("data.txt") as file:
     gameRound = item
     gameID = int((re.search(gameIDPattern, gameRound)).group(1))
 
-    matches = re.finditer(colourAndNumberPattern, gameRound)
-    max = {
+    game = { 
         "red" : 0,
         "green" : 0,
         "blue" : 0 
-    }
+        }
+
+    matches = re.finditer(colourAndNumberPattern, gameRound)
+
     for match in matches:
         number = match.group('number')
         color = match.group('color')
 
-        if int(number) > int(max[color]):
-            max[color] = int(number)
+        game[color] += int(number)
+        #print(f"Number: {number}, Color: {color}")
 
-    total += max["red"] * max["blue"] * max["green"]
-    print(max)   
-print(total)
+    for key in game.keys():
+        print("Colour " + key + ": " + str(game[key]))
+
+    if all(game[color] <= limit[color] for color in game):
+        # All colors meet their respective limits
+        print("All colors meet their limits.")
+        gameRoster[gameID]=game
+        
+    else:
+        # At least one color doesn't meet its limit
+        print("Not all colors meet their limits.")
+
+total = 0
+for key in gameRoster.keys():
+    total += key
+
+print("Sum of keys:", total)
