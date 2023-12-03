343372ccbd9722eb0357e92e8592ab64393d0cac
John Bain <john.bain@gmail.com>
2023-12-03 16:43:10 +0000

    stage1 done

---
+++ Day3/Stage1/stage1.py
@@ -0,0 +1,57 @@
+
+import numpy as np
+import re
+
+gridLeftLimit =0 
+gridTopLimit = 0
+digitPattern = r'\d+'
+symbolPattern = r'[^0-9.]'
+
+#build grid
+grid = np.genfromtxt("data.txt", dtype=str, delimiter=1,comments=None)
+
+#max limit positions
+gridRightLimit = grid.shape[0] - 1
+gridBottomLimit =grid.shape[1] - 1
+
+rowNum = 0
+numberBuilder = ""
+
+total = 0
+
+
+enginePart = False
+for row in grid:
+    
+    #print("Row: " + str(rowNum))
+    colNum = 0
+
+    for col in row:
+        #print(col)
+        
+        #check if number 
+        if col.isnumeric():
+            numberBuilder += col
+
+            #search for adjacent symbol
+            for dx in [-1, 0, 1]:
+
+                if dx+rowNum <= gridRightLimit and dx+rowNum >= gridLeftLimit:
+                    for dy in [-1, 0, 1]:
+                        if dy+colNum <= gridBottomLimit and dy+colNum >= gridTopLimit:
+                            if re.search(symbolPattern,grid[rowNum+dx][colNum+dy]):
+                                enginePart = True
+        
+        else:
+            #captured whole number
+            if numberBuilder:
+
+                if enginePart:
+                    total += int(numberBuilder)
+                    enginePart = False
+            numberBuilder = ""
+        colNum += 1
+    rowNum += 1
+
+
+print(total)
