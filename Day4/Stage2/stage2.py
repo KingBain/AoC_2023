6c954809940906856cd0d170c1d7157cf9af3a83
John Bain <john.bain@gmail.com>
2023-12-05 13:59:01 +0000

    finished stage2

--- Day4/Stage2/stage2.py
+++ Day4/Stage2/stage2.py
@@ -1,29 +1,41 @@
 import re
 
-
 gamePattern = r'^Card\s+(?P<ID>\d+):\s+(?P<WinningNumbers>[\d\s]+)\s+\|\s+(?P<MyNumbers>[\d\s]+)$'
-
+gameList = {}
 
 total = 0
 
 with open("data.txt") as file:
     for item in file:
-
-        #exampleGame = "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
         exampleGame = item 
         matches = re.finditer(gamePattern, exampleGame)
 
         for match in matches:
-            id = match.group('ID') 
+            id = int(match.group('ID')) 
             winningNumbers = match.group('WinningNumbers').split()
             myNumbers = match.group('MyNumbers').split()
-
             myWinningNumbers = list(set(winningNumbers).intersection(set(myNumbers)))
 
             if myWinningNumbers:
-                score = 2 ** (len(myWinningNumbers)-1)
+                                # wins    #copies           
+                gameList[id] = [len(myWinningNumbers),1]
             else:
-                score = 0
+                gameList[id] = [0,1]
 
-            total += score
+
+#print(gameList) 
+
+total = 0
+for i in range(1, len(gameList)+1, +1):   
+    print("card " + str(i))
+    currentCardWins = gameList[i][0]
+    currentCardCopies = gameList[i][1]
+    total += currentCardCopies
+    #print("Copied of this card " + str(currentCardCopies))
+    for c in range(0,currentCardCopies,+1):
+        for x in range(1,currentCardWins+1,+1 ):
+            nextCard = (x+i)
+            gameList[nextCard][1] += 1
+            #print("Boost card by 1, card ID " + str(nextCard))
+#print(gameList) 
 print(total)
